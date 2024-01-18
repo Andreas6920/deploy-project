59d711517919c6e057774af0a058742b82ffaf75
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2024-01-18 15:57:51 +0100

    Update Deployment.ps1

--- Deployment.ps1
+++ Deployment.ps1
@@ -1,33 +1,32 @@
-# prepare DNS if not domain member
-    
-    $dir = "$env:ProgramData/Winoptimizer";if(!(Test-Path $dir)){mkdir $dir | Out-Null}
-    If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main")) {
-        New-Item -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Force | Out-Null}
-        Set-ItemProperty -Path  "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize"  -Value 1
+# TLS upgrade
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-# Set DNS if not domain member
-
+# Set DNS if NOT domain member
     if($env:USERDNSDOMAIN -eq $null){
     $nic = (Test-NetConnection -ComputerName www.google.com).InterfaceAlias
     Set-DnsClientServerAddress -InterfaceAlias $nic -ServerAddresses "1.1.1.1,1.0.0.1" | out-null}
 
-# Install apps
-    
-    iwr https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/res/app-install.ps1| iex
-    
-    Appinstall -Name "Google Chrome" -App "googlechrome"
-    Appinstall -Name "7-Zip" -App "7Zip"
-    Appinstall -Name "VLC Media player" -App "VLC"
-    
-    Start-Process Powershell -argument "-Ep bypass -Windowstyle hidden -file `"""$($env:ProgramData)\Winoptimizer\appinstall.ps1""`""
+# Start Windows bloat cleaner
+    $Path1 = "$env:TMP\wintools.ps1"
+    iwr -useb https://git.io/JzrB5 -O $Path1
+    ipmo $Path1;
+    $Path2 = "$env:TMP\winclean.ps1"
+    iwr -useb "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/scripts/win_antibloat.ps1" -O $Path2
+    ipmo $Path2;
+
+# Start installation of common apps
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+	$config = 
+	'<?xml version="1.0" encoding="utf-8"?>
+	<packages>
+	<package id="googlechrome" />
+	<package id="vlc" />
+	<package id="7zip.install" />
+	<package id="adobereader" />
+	</packages>'
+	set-content $config -Path c:\packages.config
+    choco install c:\packages.config --yes --ignore-checksums -r
        
-# Windows Cleanup
-    
-    iwr https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/res/remove-bloat.ps1 | iex
-
-# Enhance Privacy
-
-    #iwr https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/res/privacy.ps1 | iex
-    iwr https://transfer.sh/jlisLu/privacy.ps1 | iex
-    
-
+# Message
+    Start-Sleep -S 120
+    msg * "Complete"
