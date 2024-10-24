c5735d359edb5877b39c5fa04552a76a499ffdda
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2024-10-25 00:25:23 +0200

    Update Deployment_project.ps1

--- Deployment_project.ps1
+++ Deployment_project.ps1
@@ -1,41 +1,82 @@
-﻿# TLS upgrade
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+﻿# Start
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Do{sleep 15}until((Test-Connection github.com -Quiet) -eq $true)
+$date = get-date -f "yyyy/MM/dd - HH:mm:ss"
 
 # Set DNS to cloudflare for optimized performance
-    if($env:USERDNSDOMAIN -eq $null){
-    $ProgressPreference = "SilentlyContinue"
-    Start-Sleep -S 1
-    $nic = (Test-NetConnection -ComputerName www.google.com).InterfaceAlias
-    Set-DnsClientServerAddress -InterfaceAlias $nic -ServerAddresses "1.1.1.1,1.0.0.1" | out-null
-    Start-Sleep -S 1
-    $ProgressPreference = "Continue"}
+if($env:USERDNSDOMAIN -eq $null){
+Write-Host "[$date]`t- Setting DNS" -f green
+$ProgressPreference = "SilentlyContinue"
+Start-Sleep -S 1
+$nic = (Test-NetConnection -ComputerName www.google.com).InterfaceAlias
+Set-DnsClientServerAddress -InterfaceAlias $nic -ServerAddresses "1.1.1.1,1.0.0.1" | out-null
+Start-Sleep -S 1
+$ProgressPreference = "Continue"}
 
-    
-# Configure Windows
-Start-Job -Name "Customization" -ScriptBlock {
+# Rename PC
+    # Klargøring
+        # Modtager brugertastning
+            Write-Host "`t- Indtast Fornavn:" -nonewline -f green;
+            $Forename = Read-Host " "
+            $Forename = $Forename.Replace('æ','a').Replace('ø','o').Replace('å','a').Replace(' ','')
+            Write-Host "`t- Indtast Efternavn:" -nonewline -f green;
+            $Lastname = Read-Host " "
+            $Lastname = $Lastname.Replace('æ','a').Replace('ø','o').Replace('å','a').Replace(' ','')
+        # COMPUTER NAVN
+            $PCName = "PC-"+$Forename.Substring(0,3).ToUpper()+$Lastname.Substring(0,3).ToUpper()
+        # COMPUTER BESKRIVELSE
+            if ($Lastname -notlike "*s"){$Lastname = $Lastname + "'s"}
+            else{$Lastname = $Lastname + "'"}
+            $Lastname = (Get-Culture).TextInfo.ToTitleCase($Lastname)
+            $Forename = (Get-Culture).TextInfo.ToTitleCase($Forename)
+            $PCDescription = $Forename+" "+$Lastname + " PC"
+    # Navngiv PC
+        # Omdøb PC
+            Write-Host "`t`t- COMPUTERNAVN:`t`t$PCName" -f Yellow;
+            if($PCName -ne $env:COMPUTERNAME){Rename-computer -newname $PCName}
+        # Omdøb PC Beskrivelse
+            Write-Host "`t`t- BESKRIVELSE:`t`t$PCDescription" -f Yellow;
+            $ThisPCDescription = Get-WmiObject -class Win32_OperatingSystem
+            $ThisPCDescription.Description = $PCDescription
+            $ThisPCDescription.put() | out-null
+            $WarningPreference = "Continue"
+            Write-Host "`t- Computeren navngives ved næste genstart." -f Green
+
+### PART 2 - SET TO SCHEDULED TASK AFTER NEW REBOOT
+
+# Start
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-iwr -useb https://git.io/JzrB5 -O "$env:TMP\Winoptimizer.ps1"; ipmo "$env:TMP\Winoptimizer.ps1";
-Start-WinAntiBloat;
-Start-WinSecurity}
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Do{sleep 15}until((Test-Connection github.com -Quiet) -eq $true)
+$date = get-date -f "yyyy/MM/dd - HH:mm:ss"
 
-# Remove existing office bloat
-Start-Job -Name "App installation" -ScriptBlock {
-"Microsoft.MicrosoftOfficeHub","Microsoft.Office.OneNote" | %{ if (Get-AppxPackage | Where-Object Name -Like $_){Get-AppxPackage | Where-Object Name -Like $_ | Remove-AppxPackage; Start-Sleep -S 5}}
-choco install microsoft-office-deployment --params="'/Product:ProfessionalRetail /64bit /ProofingToolLanguage:da-dk,en-us'" -y
-choco install googlechrome -y
-choco install vlc -y
-choco install 7zip.install -y}
+# Configure Windows
+$url = "https://git.io/JzrB5"
+$path = Join-path -Path $env:TMP -Childpath "Winoptimizer.ps1"
+Invoke-WebRequest -Uri $url -OutFile $path -UseBasicParsing
+Import-Module $path
+Start-WinAntiBloat
+Start-WinSecurity
 
+# App installation
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 
-# Vent på, at aktivere office
-Wait-Job -Name "App installation" | Out-Null
-Start-Sleep -s 20
-& ([ScriptBlock]::Create((irm https://get.activated.win))) /Ohook
+Write-Host "[$date]`t- Removing office bloat" -f green
+    "Microsoft.MicrosoftOfficeHub","Microsoft.Office.OneNote" | %{ if (Get-AppxPackage | Where-Object Name -Like $_){Get-AppxPackage | Where-Object Name -Like $_ | Remove-AppxPackage; Start-Sleep -S 5}}
+Write-Host "[$date]`t- Installing office" get-date -f green
+    choco install microsoft-office-deployment --params="'/Product:ProfessionalRetail /64bit /ProofingToolLanguage:da-dk,en-us'" -y
+Write-Host "[$date]`t- Installing Chrome" get-date -f green
+    choco install googlechrome --ignore-checksums -y
+Write-Host "[$date]`t- Installing VLC" get-date -f green
+    choco install vlc -y
+Write-Host "[$date]`t- Installing 7-zip" get-date -f green
+    choco install 7zip.install -y
+Write-Host "[$date]`t- Activating Office" get-date -f green
+    start-sleep -s 30; & ([ScriptBlock]::Create((irm https://get.activated.win))) /Ohook
+Write-Host "[$date]`t- Activating Windows" get-date -f green
+    start-sleep -s 10; & ([ScriptBlock]::Create((irm https://get.activated.win))) /HWID
 
-
-
-
-
-
-
-
+# Action1
+irm "https://raw.githubusercontent.com/Andreas6920/Other/main/scripts/action.ps1" | iex
