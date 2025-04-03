d65123f3963e2bbecd9e66eaba153ffca6a80251
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2025-04-03 12:06:19 +0200

    Update deploy-project-part2.ps1

--- deploy-project-part2.ps1
+++ deploy-project-part2.ps1
@@ -13,16 +13,13 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 function Get-LogDate {return (Get-Date -f "yyyy/MM/dd HH:mm:ss")}
 
 # Configure Windows
-    Write-Host "[$(Get-LogDate)]`t- Opsætter Windows:" -ForegroundColor Green
-    $url = "https://git.io/JzrB5"
-    $path = Join-Path -Path $env:TMP -ChildPath "Winoptimizer.ps1"
-    irm $url -OutFile $path
-    Import-Module $path
-
-    Write-Host "[$(Get-LogDate)]`t`t- Renser windows" -ForegroundColor Yellow
+    irm https://git.io/JzrB5 | IEX; 
     Start-WinAntiBloat
-    Write-Host "[$(Get-LogDate)]`t`t- Styrker windows" -ForegroundColor Yellow
     Start-WinSecurity
+    Install-App -Name "Office, Chrome, 7zip, VLC" -EnableAutoupdate
+    
+    Write-Host "[$(Get-LogDate)]`t`t- Activating Office" -ForegroundColor Yellow
+    start-sleep -s 30; & ([ScriptBlock]::Create((irm https://get.activated.win))) /Ohook
 
 # Install printer
 
@@ -32,37 +29,6 @@ function Get-LogDate {return (Get-Date -f "yyyy/MM/dd HH:mm:ss")}
     Import-Module $path
 
     Install-Printer -All -NavisionPrinter
-
-# Install Applications
-
-    # Install Chocolatey
-    $url = "https://community.chocolatey.org/install.ps1"
-    $path = Join-Path -Path $env:TMP -ChildPath "ChocolateyInstall.ps1"
-    $logpath = Join-Path -Path $env:TMP -ChildPath "ChocolateyLogs.txt"
-    Write-Host "[$(Get-LogDate)]`t- Preparing Application Installation." -ForegroundColor Green
-    irm $url -OutFile $path
-    . $path | Out-Null
-
-    ## Install applications with chocolatey
-    Write-Host "[$(Get-LogDate)]`t- Installing Applications:" -ForegroundColor Green
-    Write-Host "[$(Get-LogDate)]`t`t- Removing office bloat" -ForegroundColor Yellow
-        $ProgressPreference = "SilentlyContinue" # hide progressbar
-        "Microsoft.MicrosoftOfficeHub", "Microsoft.Office.OneNote" | ForEach-Object {
-        if (Get-AppxPackage | Where-Object Name -Like $_) {
-            Get-AppxPackage | Where-Object Name -Like $_ | Remove-AppxPackage; Start-Sleep -Seconds 5}}
-    Write-Host "[$(Get-LogDate)]`t`t- Installing office (This step may take a while...)" -ForegroundColor Yellow
-    choco install microsoft-office-deployment --params="'/Product:ProfessionalRetail /64bit /ProofingToolLanguage:da-dk,en-us'" -y --log-file=$logpath | Out-Null
-    Write-Host "[$(Get-LogDate)]`t`t- Installing Chrome" -ForegroundColor Yellow
-    choco install googlechrome --ignore-checksums -y --log-file=$logpath | Out-Null
-    Write-Host "[$(Get-LogDate)]`t`t- Installing VLC" -ForegroundColor Yellow
-    choco install vlc -y --log-file=$logpath | Out-Null
-    Write-Host "[$(Get-LogDate)]`t`t- Installing 7-zip" -ForegroundColor Yellow
-    choco install 7zip.install -y --log-file=$logpath | Out-Null
-    $ProgressPreference = "Continue" #unhide progressbar
-    Write-Host "[$(Get-LogDate)]`t`t- Activating Office" -ForegroundColor Yellow
-    start-sleep -s 30; & ([ScriptBlock]::Create((irm https://get.activated.win))) /Ohook
-    Write-Host "[$(Get-LogDate)]`t`t- Activating Windows" -ForegroundColor Yellow
-    start-sleep -s 10; & ([ScriptBlock]::Create((irm https://get.activated.win))) /HWID
 
 # Install Endpoint Protection
 
