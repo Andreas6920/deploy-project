b409ecc946627f92f4de6243216e510e7e5b721d
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2025-04-20 23:02:14 +0200

    Update deployment-general.ps1

--- deployment-general.ps1
+++ deployment-general.ps1
@@ -1,61 +1,41 @@
-# Start
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-[System.Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 -bor ([System.Net.ServicePointManager]::SecurityProtocol)
-Do { sleep 15 } until ((Test-Connection github.com -Quiet) -eq $true)
+# Ensure admin rights
+    If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){
+    
+# Relaunch as an elevated process
+    $Script = $MyInvocation.MyCommand.Path
+    Start-Process powershell.exe -Verb RunAs -ArgumentList "-ExecutionPolicy RemoteSigned", "-File `"$Script`""}
 
-# Funktion til at få det aktuelle tidspunkt
-function Get-LogDate {return (Get-Date -f "[yyyy/MM/dd HH:MM:ss]")}
+# Bypass ExecutionPolicy
+    Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+
+# Timestamps for actions
+    Function Get-LogDate {return (Get-Date -f "[yyyy/MM/dd HH:mm:ss]")}
+
+# Wait for internet
+    Write-Host "$(Get-LogDate)`t    Venter på internet" -ForegroundColor Green -NoNewline
+    do{Write-Host "." -ForegroundColor Green -NoNewline; sleep 3}until((Test-Connection github.com -Quiet) -eq $true)
+    Write-host " [VERIFICERET]" -ForegroundColor Green
+
+# Start Script
+    Write-Host "$(Get-LogDate)`tOPSÆTNING STARTER" -f Green
+
+# Opgrader TLS
+    Write-Host "$(Get-LogDate)`t    Opgradere TLS." -ForegroundColor Green
+    [System.Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 -bor ([System.Net.ServicePointManager]::SecurityProtocol)
+
+# Set DNS to cloudflare for optimized performance
+    Write-Host "$(Get-LogDate)`t    Opsætter DNS til Cloudflare." -ForegroundColor Green
+    if($env:USERDNSDOMAIN -eq $null){
+        $job = Start-Job -ScriptBlock {
+        $nic = (Test-NetConnection -ComputerName www.google.com).InterfaceAlias
+        Set-DnsClientServerAddress -InterfaceAlias $nic -ServerAddresses "1.1.1.1,1.0.0.1" | Out-Null}}
 
 # Configure Windows
-    $url = "https://git.io/JzrB5"
-    $path = Join-Path -Path $env:TMP -ChildPath "Winoptimizer.ps1"
-    irm $url -OutFile $path
-    . $path
-
+    irm https://git.io/JzrB5 | IEX; 
     Start-WinAntiBloat
+    Start-WinSettings
     Start-WinSecurity
-
-# Install printer
-    $url = "https://git.io/JzrB5"
-    $path = Join-Path -Path $env:TMP -ChildPath "Printer-Installation.ps1"
-    irm $url -OutFile $path
-    . $path
-
-    Install-Printer -All -NavisionPrinter
-
-# Install Applications
-
-    # Install Chocolatey
-    $url = "https://community.chocolatey.org/install.ps1"
-    $path = Join-Path -Path $env:TMP -ChildPath "ChocolateyInstall.ps1"
-    Write-Host "[$(Get-LogDate)]`t- Preparing Application Installation." -ForegroundColor Green
-    irm $url -OutFile $path
-    . $path
-
-    ## Install Applications
-    Write-Host "[$(Get-LogDate)]`t- Installing Applications:" -ForegroundColor Green
-    Write-Host "[$(Get-LogDate)]`t`t- Removing office bloat" -ForegroundColor Yellow
-    "Microsoft.MicrosoftOfficeHub", "Microsoft.Office.OneNote" | ForEach-Object {
-        if (Get-AppxPackage | Where-Object Name -Like $_) {
-            Get-AppxPackage | Where-Object Name -Like $_ | Remove-AppxPackage; Start-Sleep -Seconds 5}}
-            
-    Write-Host "[$(Get-LogDate)]`t`t- Installing office (This step may take a while...)" -ForegroundColor Yellow
-    choco install microsoft-office-deployment --params="'/Product:ProfessionalRetail /64bit /ProofingToolLanguage:da-dk,en-us'" -r -y
-    Write-Host "[$(Get-LogDate)]`t`t- Installing Chrome" -ForegroundColor Yellow
-    choco install googlechrome --ignore-checksums -r -y
-    Write-Host "[$(Get-LogDate)]`t`t- Installing VLC" -ForegroundColor Yellow
-    choco install vlc -y -r
-    Write-Host "[$(Get-LogDate)]`t`t- Installing 7-zip" -ForegroundColor Yellow
-    choco install 7zip.install -y -r
-    Write-Host "[$(Get-LogDate)]`t`t- Activating Office" -ForegroundColor Yellow
-    start-sleep -s 30; & ([ScriptBlock]::Create((irm https://get.activated.win))) /Ohook
-    Write-Host "[$(Get-LogDate)]`t`t- Activating Windows" -ForegroundColor Yellow
-    start-sleep -s 10; & ([ScriptBlock]::Create((irm https://get.activated.win))) /HWID
-
-# Install Endpoint Protection
-
-# Action1
-    # irm "https://raw.githubusercontent.com/Andreas6920/Other/main/scripts/action.ps1" | iex
+    Install-App -Name "Office, Chrome, 7zip, VLC" -EnableAutoupdate
 
 # Message
     Add-Type -AssemblyName System.Windows.Forms | Out-Null
